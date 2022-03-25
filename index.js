f3d2315dcf1a8f8e4877756feec790c58125ee24
Elsie Fernandez <els.fernandez@icloud.com>
2022-03-25 13:16:10 -0700

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const drive = 45
+
+function distanceFromHqInBlocks (drive) {
+    let walk = 44
+    if (walk > drive);
+    else (drive < walk);
+
+    return 8
+}
+const run = 5
+
+function distanceFromHqInFeet (run){
+    let walk = 40
+    if (walk <run);
+
+    return 2112
+
+}
+const jog = 2
+function distanceTravelledInFeet (jog){
+    let  travel = 20
+    if (travel < jog);
+
+    return 2640
+
+}
+const start = 20
+
+function calculatesFairPrice (start, destination){   
+   return  start * destination
+
+
+}
+
